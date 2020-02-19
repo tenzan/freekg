355373b90f3f03dd54476c5114ba3231e986249a
Askar <askar.karasaev@gmail.com>
2020-02-19 17:36:47 +0900

    Updating Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,20 @@
 <template>
-  <q-page padding> </q-page>
+  <q-page padding>
+    <div>Index</div>
+    <div>Booking</div>
+    <div>Ads</div>
+  </q-page>
 </template>
 
 <script>
+import Booking from "components/Booking";
+import Ads from "components/Ads";
+
 export default {};
+
+components: {
+  Booking, Ads;
+}
 </script>
 
 <style></style>
